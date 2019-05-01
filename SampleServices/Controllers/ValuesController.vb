9d2ddb053996ccbe531e3d93304fa23156867340
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-05-01 10:32:56 +0700

    menambahkan connectionstring

--- SampleServices/Controllers/ValuesController.vb
+++ SampleServices/Controllers/ValuesController.vb
@@ -4,26 +4,36 @@ Imports System.Web.Http
 Public Class ValuesController
     Inherits ApiController
 
+    Public nama As New List(Of String)
+    Public Sub New()
+        nama.Add("Erick")
+        nama.Add("Budi")
+        nama.Add("Bambang")
+    End Sub
+
     ' GET api/values
     Public Function GetValues() As IEnumerable(Of String)
-        Return New String() {"value1", "value2"}
+        Return nama
     End Function
 
     ' GET api/values/5
-    Public Function GetValue(ByVal id As Integer) As String
+    Public Function GetValue(ByVal id As String) As String
         Return "value"
     End Function
 
+    'untuk insert
     ' POST api/values
     Public Sub PostValue(<FromBody()> ByVal value As String)
-
+        Return
     End Sub
 
+    'untuk update
     ' PUT api/values/5
     Public Sub PutValue(ByVal id As Integer, <FromBody()> ByVal value As String)
 
     End Sub
 
+    'untuk delete
     ' DELETE api/values/5
     Public Sub DeleteValue(ByVal id As Integer)
 
