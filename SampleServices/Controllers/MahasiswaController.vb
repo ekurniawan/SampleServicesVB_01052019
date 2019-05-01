fb98c1e92d5948d44bd054b59c62d327daed0de3
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-05-01 11:05:18 +0700

    menambahkan method api

---
+++ SampleServices/Controllers/MahasiswaController.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Net
+Imports System.Web.Http
+
+Namespace Controllers
+    Public Class MahasiswaController
+        Inherits ApiController
+
+        Private mhsDb As New MahasiswaDAL
+
+        ' GET: api/Mahasiswa
+        Public Function GetValues() As IEnumerable(Of Mahasiswa)
+            Return mhsDb.GetAll()
+        End Function
+
+        ' GET: api/Mahasiswa/5
+        Public Function GetValue(ByVal id As Integer) As String
+            Return "value"
+        End Function
+
+        ' POST: api/Mahasiswa
+        Public Sub PostValue(<FromBody()> ByVal value As String)
+
+        End Sub
+
+        ' PUT: api/Mahasiswa/5
+        Public Sub PutValue(ByVal id As Integer, <FromBody()> ByVal value As String)
+
+        End Sub
+
+        ' DELETE: api/Mahasiswa/5
+        Public Sub DeleteValue(ByVal id As Integer)
+
+        End Sub
+    End Class
+End Namespace
--- SampleServices/DAL/MahasiswaDAL.vb
+++ SampleServices/DAL/MahasiswaDAL.vb
@@ -1,4 +1,4 @@
-﻿Imports System.Data.SqlClient
+﻿Imports MySql.Data.MySqlClient
 Imports System.Configuration
 
 Public Class MahasiswaDAL
@@ -7,10 +7,10 @@ Public Class MahasiswaDAL
     End Function
 
     Public Function GetAll() As IEnumerable(Of Mahasiswa)
-        Using conn As New SqlConnection(GetConnString())
+        Using conn As New MySqlConnection(GetConnString())
             Dim lstMhs As New List(Of Mahasiswa)
             Dim strSql = "select * from mahasiswa order by Nama"
-            Dim cmd As New SqlCommand(strSql, conn)
+            Dim cmd As New MySqlCommand(strSql, conn)
             conn.Open()
             Dim dr = cmd.ExecuteReader()
             If dr.HasRows Then
@@ -24,6 +24,8 @@ Public Class MahasiswaDAL
             dr.Close()
             cmd.Dispose()
             conn.Close()
+
+            Return lstMhs
         End Using
     End Function
 End Class
